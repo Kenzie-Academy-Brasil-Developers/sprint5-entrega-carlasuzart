80a1de7e62dfb747f4fb7d748426d774379dbe7c
CARLA GRAZIELLE OLIVEIRA SUZART <carlagosuzart@gmail.com>
2022-08-29 14:02:52 -0300

    entrega parcial-3

--- src/routers/routes.ts
+++ src/routers/routes.ts
@@ -3,6 +3,7 @@ import userCreateController from "../controllers/users/userCreate.controller";
 import userDeleteController from "../controllers/users/userDelete.controller";
 import userListController from "../controllers/users/userList.controller";
 import userListOneController from "../controllers/users/userListOne.controller";
+import userUpdateController from "../controllers/users/userUpdate.controller";
 
 const routes = Router();
 
@@ -10,5 +11,6 @@ routes.post("", userCreateController);
 routes.get("", userListController);
 routes.get("/:id", userListOneController);
 routes.delete("/:id", userDeleteController);
+routes.patch("/:id", userUpdateController);
 
 export default routes;
