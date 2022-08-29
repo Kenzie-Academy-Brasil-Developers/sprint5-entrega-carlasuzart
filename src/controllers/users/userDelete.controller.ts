00fb047ed3d14765a306e5d9843b31af8c6c197a
CARLA GRAZIELLE OLIVEIRA SUZART <carlagosuzart@gmail.com>
2022-08-29 14:05:35 -0300

    entrega parcial 4

--- src/controllers/users/userDelete.controller.ts
+++ src/controllers/users/userDelete.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import userDeleteService from "../../services/users/userDeleteService";
+import userDeleteService from "../../services/users/userDelete.service";
 
 const userDeleteController = async (req: Request, res: Response) => {
   try {
